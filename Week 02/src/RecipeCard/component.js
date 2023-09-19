1668999d772b81d8c14a62d026b4734558ab19f0
koeefu <102726954+koeefu@users.noreply.github.com>
2023-09-19 16:51:46 -0400

    Delete Week 02 directory

--- Week 02/src/RecipeCard/component.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import './styles.css'
-
-export default function Component(props) {
-    return <div className="component">{props.children}</div>
-}
--- Week 02/src/RecipeCard/left.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import './styles.css'
-
-export default function Left(props) {
-    return <div className="left">{props.children}</div>
-}
